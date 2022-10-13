8d4626b01b9fdd3f80aa6cf074d17dad490e0457
Luca6C756361 <fuocoluke@gmail.com>
2022-10-13 13:02:28 +0200

    es_if

--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'prima_app',
+    'seconda_app'
 ]
 
 MIDDLEWARE = [
@@ -57,6 +58,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'prima_app/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'seconda_app/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
